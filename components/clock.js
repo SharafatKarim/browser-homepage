d59bcc1de62ca711316a3662d0f76cc1cafc43bb
Sharafat <93897936+SharafatKarim@users.noreply.github.com>
2022-10-14 14:38:28 +0600

    letting hour to be 12 format cycle

--- components/clock.js
+++ components/clock.js
@@ -21,6 +21,8 @@ function showClock() {
     var hours = today.getHours();
     var minutes = today.getMinutes();
     var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
     if (hours < 10) {
         hours = "0" + hours;
     }
